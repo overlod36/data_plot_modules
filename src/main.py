0c42398c66269cc95e75be1e15c2e97ae3c7da08
kirbi <Biehan@mail.ru>
2023-10-06 18:16:59 +0300

    ok, ok, chill out

--- src/main.py
+++ src/main.py
@@ -1,10 +1,7 @@
 from PyQt6.QtWidgets import QApplication
 from forms import Main_Window
-from math_module.qalgo import clusters_formation
-import random
 
 if __name__ == '__main__':
     app = QApplication([])
     window = Main_Window()
     app.exec()
-    # print(clusters_formation([[random.randint(0, 255), random.randint(0, 255), None] for _ in range(25)], [1, 2]))
